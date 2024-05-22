a4db5691bc7b120e126b85bcc4682d4ad3d96415
Emamul Mottakin <emarko3236@gmail.com>
2024-05-22 14:19:22 +0600

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          100: "#FFCF2B",
+          200: "#FF8935",
+        },
+        secondary: "#292929",
+      },
+    },
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: "1rem",
+        sm: "2rem",
+        lg: "4rem",
+        xl: "5rem",
+        "2xl": "6rem",
+      },
+    },
+  },
+  plugins: [],
+};
