e982650c2b153fddb00fe364aaf3febb48cefa4c
Emamul Mottakin <emarko3236@gmail.com>
2024-09-08 07:40:13 +0600

    project img added and design changed

--- dist/js/app.js
+++ dist/js/app.js
@@ -7,6 +7,9 @@ const lg = 1024;
 const xl = 1280;
 const xxl = 1536;
 
+/*#####################################
+----------- nav bar expend ------------
+#######################################*/
 // elements
 const hamburger = document.getElementById("hamburger");
 const navList = document.getElementById("nav-list");
@@ -23,3 +26,25 @@ navLinks.forEach((navLink) => {
     }
   });
 });
+
+/*###########################################
+----------- project modal expend ------------
+#############################################*/
+// Element
+// const vapeEcommerceUi = document.getElementById("vape-ecommerce-ui");
+// const vapeEcommerceUiModal = vapeEcommerceUi.querySelector(".modal");
+// vapeEcommerceUi.addEventListener("click", (e) => {
+//   vapeEcommerceUiModal.classList.toggle("hidden");
+// });
+// vapeEcommerceUiModal
+//   .querySelector(".modal-wrapper")
+//   .addEventListener("click", (e) => {
+//     e.stopImmediatePropagation();
+//   });
+
+// vapeEcommerceUiModal
+//   .querySelector(".modal-close-btn")
+//   .addEventListener("click", (e) => {
+//     vapeEcommerceUiModal.classList.toggle("hidden");
+//     console.log("clicked");
+//   });
